7bf91e3da7151a3f01b50d3e857c0641f41a1d9c
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:35:27 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Emma Watson lanza nueva campaña de concientización sobre la igualdad de género",
+    "autor" => "sistemas@uns",
+    "resumen" => "La famosa actriz y activista Emma Watson, conocida por su papel como Hermione Granger en la saga de Harry Potter, ha lanzado una nueva campaña global destinada a promover la igualdad de género en el lugar de trabajo. La campaña, que busca inspirar a las empresas a adoptar políticas más inclusivas, ha recibido apoyo de diversas figuras públicas y organizaciones internacionales. Watson destacó la importancia de crear espacios seguros y equitativos para las mujeres en el ámbito profesional, y cómo todos tienen un papel crucial en el cambio hacia una sociedad más justa.",
 ];
 ?>
