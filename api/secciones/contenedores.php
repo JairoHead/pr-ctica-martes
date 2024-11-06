02231fb6e67ae67216615dddbd53b083a255d952
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:32:57 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Crisis Global de Contenedores: ¿Cómo Afecta la Cadena de Suministro Mundial?",
+    "autor" => "Reuters, Agencia de Noticias Global",
+    "resumen" => "La pandemia de COVID-19 ha generado una escasez crítica de contenedores marítimos en los principales puertos del mundo, lo que está afectando gravemente las cadenas de suministro globales. Los retrasos en el transporte y los altos costos están llevando a una crisis económica mundial, que podría durar hasta el final de 2024. Las empresas buscan soluciones innovadoras para superar esta escasez.",
 ];
 ?>
