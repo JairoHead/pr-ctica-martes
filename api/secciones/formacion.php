6476d052be8c39fd5cd8d02a4735a74259507a6f
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:35:01 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+    "titulo" => "Las Nuevas Tendencias en Formación: La Educación Online Revoluciona el Acceso a la Capacitación Profesional",
+    "autor" => "sistemas@uns",
+    "resumen" => "Con el auge de las plataformas de aprendizaje online, la formación continua y el acceso a nuevas habilidades se ha democratizado, permitiendo que millones de personas a nivel mundial puedan acceder a cursos, diplomados y certificaciones de alta calidad. Las universidades y empresas están adoptando modelos híbridos que combinan la enseñanza presencial con la virtual, lo que proporciona flexibilidad para estudiantes y profesionales en su desarrollo de competencias. Sin embargo, la brecha digital sigue siendo un desafío para muchas regiones.",
 ];
 ?>
