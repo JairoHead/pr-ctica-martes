777b7a77bc87a085bebb5a2aa9dd9f641d3d0c46
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:38:14 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El impacto de la inteligencia artificial en el empleo global",
+    "autor" => "Juan Pérez, Analista Tecnológico",
+    "resumen" => "La inteligencia artificial está revolucionando sectores laborales, pero ¿cómo afectará a los empleos en las próximas décadas? En este artículo se examinan los posibles escenarios y cómo adaptarse a los cambios.",
 ];
 ?>
