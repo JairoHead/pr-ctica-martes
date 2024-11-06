ed7819df8705c797f259298c4ddc87ba6ac3c3c9
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:39:54 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El regreso de 'Friends' con una nueva serie de reencuentro",
+    "autor" => "Carlos Martínez",
+    "resumen" => "El icónico elenco de 'Friends' vuelve para una serie especial de reencuentro que promete revivir los mejores momentos de la serie.",
 ];
 ?>
