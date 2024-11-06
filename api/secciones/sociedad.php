3f90b3ef7e25e760600133f306a49bc59009a7b9
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:39:04 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El impacto de la inteligencia artificial en el empleo",
+    "autor" => "Juan Pérez",
+    "resumen" => "La automatización y la inteligencia artificial están transformando el mercado laboral, generando nuevas oportunidades pero también desafíos para los trabajadores en diversas industrias.",
 ];
 ?>
