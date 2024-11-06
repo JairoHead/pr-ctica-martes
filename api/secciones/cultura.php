894c7d5796fc5469ddebb8b936ca381112fd72d1
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:33:26 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El Retorno del Festival de Cannes: Una Edición Histórica Tras la Pandemia",
+    "autor" => "El País, Cultura",
+    "resumen" => "El Festival de Cine de Cannes regresa en 2024 con una edición histórica tras las restricciones de la pandemia. Este evento emblemático reúne a cineastas, actores y críticos de todo el mundo, destacando las películas más innovadoras y esperadas. Con nuevas medidas de seguridad y una mayor presencia internacional, el festival reafirma su papel como plataforma clave en la industria cinematográfica.",
 ];
 ?>
