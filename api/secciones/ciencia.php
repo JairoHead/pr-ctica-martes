d5f21e7a120b6638c99f9b88d23cec59bf15e3f3
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:32:02 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Avances en la Exploración de Marte: Perseverance Encuentra Signos de Vida Pasada",
+    "autor" => "NASA, Agencia Espacial de Estados Unidos",
+    "resumen" => "El rover Perseverance de la NASA ha hecho un importante descubrimiento en el cráter Jezero de Marte, encontrando pruebas que sugieren que el planeta rojo pudo haber albergado vida microbiana en su pasado. Los hallazgos se basan en muestras de rocas y sedimentos, que serán enviadas a la Tierra en futuras misiones para su análisis detallado.",
 ];
 ?>
+
