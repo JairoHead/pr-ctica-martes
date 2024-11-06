71ec8243e90c7ef5d2550a35a57768acceaa4f82
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:39:33 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La llegada de los teléfonos plegables a gran escala",
+    "autor" => "María González",
+    "resumen" => "Los smartphones plegables están revolucionando el mercado con diseños innovadores y características mejoradas, llevando la tecnología móvil a un nuevo nivel.",
 ];
 ?>
