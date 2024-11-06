e7fd96f9d370bd6f3c020867428f6341662c4e56
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:36:00 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Git 2.40 mejora el rendimiento y la seguridad en el manejo de ramas",
+    "autor" => "sistemas@uns",
+    "resumen" => "La nueva versión de Git, la 2.40, ha sido lanzada con importantes mejoras en el rendimiento y seguridad al trabajar con ramas y repositorios grandes. Entre los cambios más destacados, se incluye una optimización en la gestión de ramas remotas, que reduce significativamente el tiempo de clonación de repositorios masivos, y nuevas herramientas para detectar vulnerabilidades de seguridad en los comandos de `git clone`. Además, Git ahora incluye una mejora en su protocolo de transferencia para facilitar la sincronización de repositorios en redes inestables. Esta versión promete ser un gran avance para los desarrolladores que trabajan en proyectos colaborativos de gran escala.",
 ];
 ?>
