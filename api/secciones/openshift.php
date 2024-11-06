77c3e1890f65150f8c99808a40847c33f9d4f123
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:37:23 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Red Hat anuncia nuevas características para OpenShift 4.12",
+    "autor" => "tech@redhat.com",
+    "resumen" => "OpenShift 4.12 trae mejoras en la integración con Kubernetes y nuevas herramientas para la automatización del ciclo de vida de las aplicaciones.",
 ];
 ?>
