c5e4f089b8af42f98941a259db293fe56c18a32d
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:40:47 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El estreno de la nueva película de Marvel, 'Avengers: Endgame 2'",
+    "autor" => "Ana López",
+    "resumen" => "Marvel anuncia el estreno de 'Avengers: Endgame 2', una secuela esperada que promete llevar a los fanáticos a una nueva aventura épica.",
 ];
 ?>
