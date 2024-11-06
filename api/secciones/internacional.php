52fc51b169eab127a65489b87b912e4917a35ecf
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:36:28 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Acuerdo histórico entre Estados Unidos y la Unión Europea para combatir el cambio climático",
+    "autor" => "corresponsal@globalnews",
+    "resumen" => "En una cumbre histórica celebrada en Bruselas, Estados Unidos y la Unión Europea han alcanzado un acuerdo conjunto para combatir el cambio climático. Este pacto incluye el compromiso de ambos bloques de reducir sus emisiones de gases de efecto invernadero en un 50% para 2030 y la implementación de políticas que fomenten las energías renovables y la innovación tecnológica en el sector medioambiental. El acuerdo también promueve la cooperación internacional para apoyar a los países en vías de desarrollo a adaptarse al cambio climático. Los líderes mundiales han destacado que este acuerdo marca un paso crucial hacia una agenda global de sostenibilidad.",
 ];
 ?>
