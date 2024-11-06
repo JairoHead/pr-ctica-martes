db5105886aac7fc7acf98f601cc11c55895b2250
JairoHead <158247861+JairoHead@users.noreply.github.com>
2024-11-06 11:38:33 -0500

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Tendencias",
-"autor" => "sistemas@uns",
-"resumen" => "Las ultimas tendencias 2024",
+    "titulo" => "Las tendencias tecnológicas para 2024 que marcarán el futuro",
+    "autor" => "sistemas@uns",
+    "resumen" => "Conoce las tecnologías emergentes que dominarán el próximo año, desde la inteligencia artificial hasta la computación cuántica, y cómo transformarán la industria y la vida cotidiana.",
 ];
 ?>
