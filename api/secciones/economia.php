af5d9a2195400ce61271446b4d6771c0cd1572ea
JairoHead <158247861+JairoHead@users.noreply.github.com>
2025-02-18 12:51:03 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,5 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-    "titulo" => "La Reserva Federal de EE.UU. Mantiene las Tasas de Interés Estables en un Contexto de Crecimiento Moderado",
-    "autor" => "Reuters, Economía Global",
-    "resumen" => "La Reserva Federal de Estados Unidos ha decidido mantener las tasas de interés estables, en un esfuerzo por equilibrar la inflación y fomentar un crecimiento económico sostenido. La decisión se produce en un contexto de desaceleración en el crecimiento global y un aumento de las preocupaciones sobre las tensiones comerciales internacionales. Los mercados financieros siguen de cerca los movimientos de la Fed, ya que sus decisiones tienen un impacto global sobre los costos de los préstamos y la inversión.",
 ];
 ?>
